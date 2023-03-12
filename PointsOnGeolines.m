6b337a4a38fa12358bd123f0faf7268d5c906868
Jiangfeng <jiangfcn@163.com>
2023-03-12 16:45:21 +0800

    generate site ID automatically

--- PointsOnGeolines.m
+++ PointsOnGeolines.m
@@ -1,21 +1,23 @@
 close all; clear all; clc;
-% generating points on a geospheric line and making the points distribute with equal spacing. 
-n = 5; % number of points you want.
-filename = 'A:\Project\Research\MarianaSubz\Doc\MTsites\MR2023locs_cp.txt'; % files will be output
-% provide sites' name
-% ID = ["MR2301","MR2302","MR2303","MR2304","MR2305",...
-%         "MR2306","MR2307","MR2308","MR2309","MR2310",...
-%         "MR2311","MR2312","MR2313","MR2314","MR2315"];
-%     ,...
-ID = ["MR2316","MR2317","MR2318","MR2319","MR2320"]; % sites's name
-% P1 = [1.726530424612083,148.2032171070533];
-% P2 = [1.92349706825732,150.4759697772407];
-% P1 = [12.669000, 140.996111];
-% P2 = [11.643547, 141.898852];
-P1 = [11.183519, 142.302020]; % the two points on the endd of a line.
-P2 = [10.594834, 142.814680];
+% 
+% generating equal spacing points on a geospheric line. 
+%
+n = 20; % number of points you want.
+prefix = 'LYT';% provide sites' name
+snumb = 2300 + [1:n];
+P1 = [15.230000, 107.104293]; % the two points on the endd of a line.
+P2 = [17.240000, 107.748574];
+
 
 %% NOT CAHNGE ANYTHING BELOW=======================================
+filename = [prefix,num2str(n),'.txt']; % files will be output
+if exist(filename,'file')
+    error('The File Already Exist in This Folder');
+end
+for iid = 1:length(snumb)
+    ID(iid) = string([prefix,num2str(snumb(iid))]); 
+end
+
 geocent = [min(P1(1),P2(1)) + abs(P1(1)-P2(1)), min(P1(2),P2(2)) + abs(P1(2)-P2(2))];
 long = [P1(2);P2(2)];
 lat = [P1(1);P2(1)];
